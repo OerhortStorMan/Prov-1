d146f24d21a395457c48f7e825a219032b439f7f
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-10-14 11:07:19 +0200

    Last commit?

--- Prov/Books.cs
+++ Prov/Books.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Prov
 {
@@ -15,8 +16,10 @@ namespace Prov
 
         public void Book()
         {
-            int curseTest = generator.Next(0,100);
-            if (curseTest <= 20)
+            
+            //random om den är cursed eller ej
+            int isBookCursed = generator.Next(0,100);
+            if (isBookCursed <= 20)
             {
                 cursed = true;
             }
@@ -24,16 +27,56 @@ namespace Prov
             {
                 cursed = false;
             }
+
+            //ger en kategori
+            List<string> categories = new List<string>(new string[] {"Comedy", "Fiction", "Adventure"});
+            int randomCategory = generator.Next(0, 2);
+            category = categories[randomCategory];
+
+            int randomTitle;
+            if (category == categories[0])
+            {
+                List<string> comedyTitles = new List<string>(new string[] {"Top 100 jokes", "Evolution of comedy", "Love Winelid: An auto biography"});
+                randomTitle = generator.Next(0, 2);
+                name = comedyTitles[randomTitle];
+                
+            }
+            else if (category == categories[1])
+            {
+                List<string> fictionTitles = new List<string>(new string[] {"Star battle", "Space attack", "Surviving mars"});
+                randomTitle = generator.Next(0, 2);
+                name = fictionTitles[randomTitle];
+            }
+            else
+            {
+                List<string> adventureTitles = new List<string>(new string[] {"Jimbiana Whones", "Surviving NTI", "Road to Spain"});
+                randomTitle = generator.Next(0, 2);
+                name = adventureTitles[randomTitle];
+            }
+
+
+
+
+
+
         }
 
         public void PrintInfo()
         {
-
+            System.Console.WriteLine(@"
+            Boken heter" + name +
+            /*"Den har " + rarity + " rarity" +*/
+            
+            "Boken tillhör kategori: " + category 
+            /*+ "Boken kostar " + price*/);
+            
         }
 
         public int Evaluate()
         {
-            return 0;
+            int price = actualValue * rarity;
+            
+            return price;
         }
 
         public string GetCategory()
@@ -48,7 +91,31 @@ namespace Prov
 
         public bool IsCursed()
         {
-            return false;
+            int evaluation = generator.Next(0, 100);
+
+            if (evaluation <= 20)
+            {
+                if (cursed == false)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (cursed == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
         }
     }
 }
--- Prov/Program.cs
+++ Prov/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Prov
 {
@@ -7,6 +8,7 @@ namespace Prov
         static void Main(string[] args)
         {
             //eftersom man ska kunna köpa o sälja böcker så kör jag två separata metoder för spelet
+            System.Console.WriteLine("Välkommen till antikspelet, du spelar som en manager för bokavdelningen");
             Buying();
 
         }
@@ -16,6 +18,27 @@ namespace Prov
             bool gameIsRunning = true;
             while (gameIsRunning)
             {
+                System.Console.WriteLine("Här är tre böcker:");
+
+                Books b1 = new Books();
+                b1.Book();
+                b1.PrintInfo();
+                System.Console.WriteLine();
+                Thread.Sleep(1500);
+                Books b2 = new Books();
+                b2.Book();
+                b2.PrintInfo();
+                System.Console.WriteLine();
+                Thread.Sleep(1500);
+                Books b3 = new Books();
+                b3.Book();
+                b3.PrintInfo();
+                System.Console.WriteLine();
+
+                System.Console.WriteLine("Vilken vill du ta en närmare titt på?");
+                string input = Console.ReadLine();
+
+                
 
             }
         }
